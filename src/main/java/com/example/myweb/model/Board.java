22e8733020e2f5afaa9d72336ad2830b9b917069
Ori <leesulgi66@naver.com>
2023-09-08 20:39:50 +0900

    Board entity

---
+++ src/main/java/com/example/myweb/model/Board.java
@@ -0,0 +1,32 @@
+package com.example.myweb.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob // 대용량 데이터
+    private String content;
+
+    @ColumnDefault("0")
+    private long count;
+
+    @ManyToOne
+    @JoinColumn(name="memberID")
+    private Member user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
