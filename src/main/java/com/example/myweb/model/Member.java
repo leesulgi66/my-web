8279a0e163ea0e13d65017fb953c0699787beb94
Ori <leesulgi66@naver.com>
2023-09-09 19:31:42 +0900

    @Enumerated value 추가

--- src/main/java/com/example/myweb/model/Member.java
+++ src/main/java/com/example/myweb/model/Member.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -33,12 +34,13 @@ public class Member {
     private String nickname;
 
     @Column(nullable = false, length = 10)
+    @Enumerated(value = EnumType.STRING)
     private Role role;
 
     @CreationTimestamp
     private Timestamp createDate;
 
     public enum Role {
-        ADMIN, USER, MANAGER
+        USER, MANAGER, ADMIN
     }
 }
