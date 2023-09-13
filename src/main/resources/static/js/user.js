d3a15d4433d842ba107f7250a67fbe9699bec11d
Ori <leesulgi66@naver.com>
2023-09-13 20:10:29 +0900

    user.js - ajax 요청

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,12 +1,33 @@
 let index = {
     init: function() {
-        $("#btn-save").on("click", ()=>{
+        $("#btn-save").on("click", ()=>{  // 화살표 함수 -> JavaScript 함수의 this 바인딩
             this.save();
         })
     },
 
     save: function() {
-        alert("user의 save함수 호출됨");
+        let data = {
+            username : $("#username").val(),
+            nickname : $("#nickname").val(),
+            password : $("#password").val(),
+            password_re : $("#password-re").val(),
+            email : $("#email").val(),
+        }
+
+        $.ajax({
+            type:"POST",
+            url: "/web/api/user/",
+            data: JSON.stringify(data),  // body date
+            contentType: "application/json; charset=utf-8", // dody data type(MIME)
+            dataType: "json" // reponse가 json 형식이라면 javascript로 변경해줌
+        }).done(function(resp){
+            alert("회원가입이 완료 되었습니다.");   
+            alert(resp);
+            location.href="/web";
+
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
     }
 }
 
