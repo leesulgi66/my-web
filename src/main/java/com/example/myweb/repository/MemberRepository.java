f9003337af71d3c7fb2dae799bfdd17b17fbc309
Ori <leesulgi66@naver.com>
2023-09-15 20:40:47 +0900

    MemberRepository - user login repository

--- src/main/java/com/example/myweb/repository/MemberRepository.java
+++ src/main/java/com/example/myweb/repository/MemberRepository.java
@@ -2,7 +2,10 @@ package com.example.myweb.repository;
 
 import com.example.myweb.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Member findByUsernameAndPassword(String username, String password);
+//    @Query(value = "SELECT * FROM member WHERE username = ?1 AND password = ?2", nativeQuery = true)
+//    Member login(String username, String password);   // findByUsernmaeAndPassword와 동일
 }
