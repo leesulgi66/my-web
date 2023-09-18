41f799cca305f7b370c42a94658a2032d34ed407
Ori <leesulgi66@naver.com>
2023-09-18 17:42:26 +0900

    UserRepository - 변경

--- src/main/java/com/example/myweb/repository/UserRepository.java
+++ src/main/java/com/example/myweb/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.example.myweb.repository;
 import com.example.myweb.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
 }
 
-//    Member findByUsernameAndPassword(String username, String password);
+//    User findByUsernameAndPassword(String username, String password);
 //    @Query(value = "SELECT * FROM member WHERE username = ?1 AND password = ?2", nativeQuery = true)
-//    Member login(String username, String password);   // findByUsernmaeAndPassword와 동일
+//    User login(String username, String password);   // findByUsernmaeAndPassword와 동일
