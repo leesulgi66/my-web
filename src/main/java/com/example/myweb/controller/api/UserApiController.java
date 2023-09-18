4b033fe4b664e53b689f9248e28ad73974c2bafe
Ori <leesulgi66@naver.com>
2023-09-18 17:45:50 +0900

    UserApiController - refactoring

--- src/main/java/com/example/myweb/controller/api/UserApiController.java
+++ src/main/java/com/example/myweb/controller/api/UserApiController.java
@@ -2,7 +2,7 @@ package com.example.myweb.controller.api;
 
 import com.example.myweb.dto.ResponseDto;
 import com.example.myweb.model.User;
-import com.example.myweb.service.MemberService;
+import com.example.myweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     @Autowired
-    private MemberService memberService;
+    private UserService userService;
 
     @PostMapping("/auth/join")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController : save 호출됨");
-        memberService.save(user);
+        userService.save(user);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 }
