d20fe6e7b0547cb9486cbc89aad7dc6364fdbd74
Ori <leesulgi66@naver.com>
2023-09-18 17:46:06 +0900

    UserService - refactoring

--- src/main/java/com/example/myweb/service/UserService.java
+++ src/main/java/com/example/myweb/service/UserService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-public class MemberService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
