d014f04ad06d0609ac462993075a4b31736ab7a0
Ori <leesulgi66@naver.com>
2023-09-18 17:47:55 +0900

    SecurityConfig

--- src/main/java/com/example/myweb/config/SecurityConfig.java
+++ src/main/java/com/example/myweb/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.example.myweb.config;
 
+import com.example.myweb.config.auth.PrincipalDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    PrincipalDetailService principalDetailService;
 
     @Bean
     BCryptPasswordEncoder encode() {
