0415e18ff197c60f7d0025c706e7049434140d56
Ori <leesulgi66@naver.com>
2023-09-22 16:24:29 +0900

    BoardController - boardList

--- src/main/java/com/example/myweb/controller/BoardController.java
+++ src/main/java/com/example/myweb/controller/BoardController.java
@@ -1,13 +1,20 @@
 package com.example.myweb.controller;
 
+import com.example.myweb.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("boards", boardService.boardList());
         return "index";
     }
 
