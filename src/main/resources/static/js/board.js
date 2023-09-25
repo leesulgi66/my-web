4c6791582bfbf37b174063b6688d2a06dcd32d8a
Ori <leesulgi66@naver.com>
2023-09-25 19:28:12 +0900

    board.js - 글 삭제 ajax 요청

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,10 @@ let index = {
         $("#board-save").on("click", ()=>{  // 화살표 함수 -> JavaScript 함수의 this 바인딩
             this.save();
         });
+
+        $("#board-delete").on("click", ()=>{  // 화살표 함수 -> JavaScript 함수의 this 바인딩
+                    this.deleteById();
+                });
     },
 
     save: function() {
@@ -47,6 +51,40 @@ let index = {
             alert(JSON.stringify(error));
         });
     },
+
+    deleteById : function() {
+            let id = $("#id").text();
+
+            if (!confirm("정말 글을 삭제할까요?")) {
+                return false;
+            } else {
+                $.ajax({
+                    type:"DELETE",
+                    url: "/api/board/"+id,
+                    dataType: "json",
+                    success: function (data) {
+                            console.log(data)
+                        },
+                    error: function (request, status, error) {
+                        console.log("code: " + request.status)
+                        console.log("message: " + request.responseText)
+                        console.log("error: " + error);
+                    }
+                }).done(function(resp){
+                    console.log(resp);
+                    if(resp.status == 500) {
+                        //alert(resp.data);
+                        alert("삭제가 실패했습니다.");
+                        location.href="/auth/joinForm";
+                    }else {
+                        alert("삭제가 완료 되었습니다.");
+                        location.href="/";
+                    }
+                }).fail(function(error){
+                    alert(JSON.stringify(error));
+                });
+            }
+        },
 }
 
 index.init();
