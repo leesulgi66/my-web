479c348e2d68f14a1f4be6ecf77970439766a744
Ori <leesulgi66@naver.com>
2023-09-28 17:19:20 +0900

    UserApiController - update() addition

--- src/main/java/com/example/myweb/controller/api/UserApiController.java
+++ src/main/java/com/example/myweb/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import com.example.myweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class UserApiController {
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController : save 호출됨");
         userService.save(user);
+        return new ResponseDto<>(HttpStatus.OK.value(), 1);
+    }
+
+    @PutMapping("/user/info")
+    public ResponseDto<Integer> update(@RequestBody User user) {
+        userService.update(user);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 }
