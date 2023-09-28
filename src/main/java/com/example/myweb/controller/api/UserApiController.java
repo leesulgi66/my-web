2f0ba8ff390986a4ee19e84d9bd36e06c0cfd0ad
Ori <leesulgi66@naver.com>
2023-09-28 19:42:36 +0900

    UserApiController - @AuthenticationPrincipal add

--- src/main/java/com/example/myweb/controller/api/UserApiController.java
+++ src/main/java/com/example/myweb/controller/api/UserApiController.java
@@ -1,10 +1,12 @@
 package com.example.myweb.controller.api;
 
+import com.example.myweb.config.auth.PrincipalDetail;
 import com.example.myweb.dto.ResponseDto;
 import com.example.myweb.model.User;
 import com.example.myweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,8 +26,8 @@ public class UserApiController {
     }
 
     @PutMapping("/user/info")
-    public ResponseDto<Integer> update(@RequestBody User user) {
-        userService.update(user);
+    public ResponseDto<Integer> update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        userService.update(user, principalDetail); // 업데이트된 유저 정보를 저장
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 }
