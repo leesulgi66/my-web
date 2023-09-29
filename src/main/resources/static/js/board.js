186696db24f83b9e8f231cfa798f30a039b60ee2
Ori <leesulgi66@naver.com>
2023-09-29 21:49:34 +0900

    board.js - delete method

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -4,12 +4,12 @@ let index = {
             this.save();
         });
 
-        $("#board-delete").on("click", ()=>{  // 화살표 함수 -> JavaScript 함수의 this 바인딩
-                    this.deleteById();
+        $("#board-delete").on("click", ()=>{
+            this.deleteById();
         });
 
-        $("#board-update").on("click", ()=>{  // 화살표 함수 -> JavaScript 함수의 this 바인딩
-                            this.update();
+        $("#board-update").on("click", ()=>{
+            this.update();
         });
     },
 
@@ -86,53 +86,54 @@ let index = {
                     }
                 }).fail(function(error){
                     alert(JSON.stringify(error));
-                });
+            });
+        }
+    },
+
+    update: function() {
+            let id = $("#id").val();
+            console.log(id);
+            let data = {
+                title : $("#title").val(),
+                content : $("#content").val()
             }
-        },
+            if(data.title == "") {
+                alert("제목을 입력해 주세요");
+                return;
+            }
+            if(data.content == "") {
+                alert("내용을 입력해 주세요");
+                return;
+            }
 
-        update: function() {
-                let data = {
-                    id : $("#id").val(),
-                    title : $("#title").val(),
-                    content : $("#content").val()
+            $.ajax({
+                type:"PUT",
+                url: "/api/board/"+id,
+                data: JSON.stringify(data),  // body date
+                contentType: "application/json; charset=utf-8", // dody data type(MIME)
+                dataType: "json", // reponse가 json 형식이라면 javascript로 변경해줌
+                success: function (data) {
+                        console.log(data)
+                    },
+                error: function (request, status, error) {
+                    console.log("code: " + request.status)
+                    console.log("message: " + request.responseText)
+                    console.log("error: " + error);
                 }
-                if(data.title == "") {
-                    alert("제목을 입력해 주세요");
-                    return;
+            }).done(function(resp){
+                console.log(resp);
+                if(resp.status == 500) {
+                    //alert(resp.data);
+                    alert("글수정이 실패했습니다.");
+                    location.href="/board/"+id;
+                }else {
+                    alert("글수정이 완료 되었습니다.");
+                    location.href="/";
                 }
-                if(data.content == "") {
-                    alert("내용을 입력해 주세요");
-                    return;
-                }
-
-                $.ajax({
-                    type:"PUT",
-                    url: "/api/board/"+data.id,
-                    data: JSON.stringify(data),  // body date
-                    contentType: "application/json; charset=utf-8", // dody data type(MIME)
-                    dataType: "json", // reponse가 json 형식이라면 javascript로 변경해줌
-                    success: function (data) {
-                            console.log(data)
-                        },
-                    error: function (request, status, error) {
-                        console.log("code: " + request.status)
-                        console.log("message: " + request.responseText)
-                        console.log("error: " + error);
-                    }
-                }).done(function(resp){
-                    console.log(resp);
-                    if(resp.status == 500) {
-                        //alert(resp.data);
-                        alert("글수정이 실패했습니다.");
-                        location.href="/auth/joinForm";
-                    }else {
-                        alert("글수정이 완료 되었습니다.");
-                        location.href="/";
-                    }
-                }).fail(function(error){
-                    alert(JSON.stringify(error));
-                });
-            },
+            }).fail(function(error){
+                alert(JSON.stringify(error));
+        });
+    },
 }
 
 index.init();
