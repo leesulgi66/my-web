a4cbd8629fd14510beff3dcc1b6fd2216616152a
Ori <leesulgi66@naver.com>
2023-10-02 22:45:04 +0900

    board.js - replySave();

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -11,6 +11,10 @@ let index = {
         $("#board-update").on("click", ()=>{
             this.update();
         });
+
+        $("#btn-reply-save").on("click", ()=>{
+            this.replySave();
+        });
     },
 
     save: function() {
@@ -57,61 +61,15 @@ let index = {
     },
 
     deleteById : function() {
-            let id = $("#id").text();
+        let id = $("#id").text();
 
-            if (!confirm("정말 글을 삭제할까요?")) {
-                return false;
-            } else {
-                $.ajax({
-                    type:"DELETE",
-                    url: "/api/board/"+id,
-                    dataType: "json",
-                    success: function (data) {
-                            console.log(data)
-                        },
-                    error: function (request, status, error) {
-                        console.log("code: " + request.status)
-                        console.log("message: " + request.responseText)
-                        console.log("error: " + error);
-                    }
-                }).done(function(resp){
-                    console.log(resp);
-                    if(resp.status == 500) {
-                        //alert(resp.data);
-                        alert("삭제가 실패했습니다.");
-                        location.href="/auth/joinForm";
-                    }else {
-                        alert("삭제가 완료 되었습니다.");
-                        location.href="/";
-                    }
-                }).fail(function(error){
-                    alert(JSON.stringify(error));
-            });
-        }
-    },
-
-    update: function() {
-            let id = $("#id").val();
-            console.log(id);
-            let data = {
-                title : $("#title").val(),
-                content : $("#content").val()
-            }
-            if(data.title == "") {
-                alert("제목을 입력해 주세요");
-                return;
-            }
-            if(data.content == "") {
-                alert("내용을 입력해 주세요");
-                return;
-            }
-
+        if (!confirm("정말 글을 삭제할까요?")) {
+            return false;
+        } else {
             $.ajax({
-                type:"PUT",
+                type:"DELETE",
                 url: "/api/board/"+id,
-                data: JSON.stringify(data),  // body date
-                contentType: "application/json; charset=utf-8", // dody data type(MIME)
-                dataType: "json", // reponse가 json 형식이라면 javascript로 변경해줌
+                dataType: "json",
                 success: function (data) {
                         console.log(data)
                     },
@@ -124,14 +82,101 @@ let index = {
                 console.log(resp);
                 if(resp.status == 500) {
                     //alert(resp.data);
-                    alert("글수정이 실패했습니다.");
-                    location.href="/board/"+id;
+                    alert("삭제가 실패했습니다.");
+                    location.href="/auth/joinForm";
                 }else {
-                    alert("글수정이 완료 되었습니다.");
+                    alert("삭제가 완료 되었습니다.");
                     location.href="/";
                 }
             }).fail(function(error){
                 alert(JSON.stringify(error));
+             });
+        }
+    },
+
+    update: function() {
+        let id = $("#id").val();
+        console.log(id);
+        let data = {
+            title : $("#title").val(),
+            content : $("#content").val()
+        }
+        if(data.title == "") {
+            alert("제목을 입력해 주세요");
+            return;
+        }
+        if(data.content == "") {
+            alert("내용을 입력해 주세요");
+            return;
+        }
+
+        $.ajax({
+            type:"PUT",
+            url: "/api/board/"+id,
+            data: JSON.stringify(data),  // body date
+            contentType: "application/json; charset=utf-8", // dody data type(MIME)
+            dataType: "json", // reponse가 json 형식이라면 javascript로 변경해줌
+            success: function (data) {
+                    console.log(data)
+                },
+            error: function (request, status, error) {
+                console.log("code: " + request.status)
+                console.log("message: " + request.responseText)
+                console.log("error: " + error);
+            }
+        }).done(function(resp){
+            console.log(resp);
+            if(resp.status == 500) {
+                //alert(resp.data);
+                alert("글수정이 실패했습니다.");
+                location.href="/board/"+id;
+            }else {
+                alert("글수정이 완료 되었습니다.");
+                location.href="/";
+            }
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+
+    replySave : function() {
+        let data = {
+            boardId : $("#id").text(),
+            content : $("#reply-content").val()
+        }
+        console.log(data);
+
+        if(data.content == "") {
+            alert("내용을 입력해 주세요");
+            return;
+        }
+
+        $.ajax({
+            type:"POST",
+            url: `/api/board/${data.boardId}/reply`,
+            data: JSON.stringify(data),  // body date
+            contentType: "application/json; charset=utf-8", // dody data type(MIME)
+            dataType: "json", // reponse가 json 형식이라면 javascript로 변경해줌
+            success: function (data) {
+                    console.log(data)
+                },
+            error: function (request, status, error) {
+                console.log("code: " + request.status)
+                console.log("message: " + request.responseText)
+                console.log("error: " + error);
+            }
+        }).done(function(resp){
+            console.log(resp);
+            if(resp.status == 500) {
+                //alert(resp.data);
+                alert("댓글작성이 실패했습니다.");
+                location.href=`/board/${data.boardId}`;
+            }else {
+                alert("댓글작성이 완료 되었습니다.");
+                location.href=`/board/${data.boardId}`;
+            }
+        }).fail(function(error){
+            alert(JSON.stringify(error));
         });
     },
 }
