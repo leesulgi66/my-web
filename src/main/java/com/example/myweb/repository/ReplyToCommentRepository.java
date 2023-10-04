12bf5f08c5b156214dd0c2d7608509aa81afefe9
Ori <leesulgi66@naver.com>
2023-10-04 22:59:00 +0900

    ReplyToCommentRepository

--- src/main/java/com/example/myweb/repository/ReplyToCommentRepository.java
+++ src/main/java/com/example/myweb/repository/ReplyToCommentRepository.java
@@ -3,6 +3,6 @@ package com.example.myweb.repository;
 import com.example.myweb.model.ReplyToComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReplytoCommentRepository extends JpaRepository<ReplyToComment, Long> {
+public interface ReplyToCommentRepository extends JpaRepository<ReplyToComment, Long> {
     void deleteAllByUserId(Long id);
 }
