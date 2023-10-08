096e1fa633d752afe38574c3b9f50bec94a93b3e
Ori <leesulgi66@naver.com>
2023-10-09 04:33:48 +0900

    user.js - imgUpload()

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -16,6 +16,10 @@ let index = {
             this.deleteForm();
         });
 
+        $("#btn-profile-image").on("click", ()=>{
+            this.imgUpload();
+        });
+
         $("#email").on("keydown", (key)=>{
             if(key.keyCode==13) {
                 this.save();
@@ -95,60 +99,60 @@ let index = {
     },
 
     update: function() {
-            let data = {
-                id: $("#id").val(),
-                nickname : $("#nickname").val(),
-                password : $("#password").val(),
-                password_re : $("#password-re").val(),
-                email : $("#email-update").val(),
-            }
+        let data = {
+            id: $("#id").val(),
+            nickname : $("#nickname").val(),
+            password : $("#password").val(),
+            password_re : $("#password-re").val(),
+            email : $("#email-update").val(),
+        }
 
-            if(data.nickname == "") {
-                alert("닉네임을 입력해 주세요");
-                return;
-            }
-            if(data.password == "") {
-                alert("비밀번호를 입력해 주세요");
-                return;
-            }
-            if(data.password_re == "") {
-                alert("비밀번호 확인을 입력해 주세요");
-                return;
-            }
-            if(data.email == "") {
-                alert("이메일을 입력해 주세요");
-                return;
-            }
+        if(data.nickname == "") {
+            alert("닉네임을 입력해 주세요");
+            return;
+        }
+        if(data.password == "") {
+            alert("비밀번호를 입력해 주세요");
+            return;
+        }
+        if(data.password_re == "") {
+            alert("비밀번호 확인을 입력해 주세요");
+            return;
+        }
+        if(data.email == "") {
+            alert("이메일을 입력해 주세요");
+            return;
+        }
 
-            if(data.password !== data.password_re) {
-                alert("비밀번호가 일치하지 않습니다.")
-                return;
-            }
+        if(data.password !== data.password_re) {
+            alert("비밀번호가 일치하지 않습니다.")
+            return;
+        }
 
-            $.ajax({
-                type:"PUT",
-                url: "/user/info",
-                data: JSON.stringify(data),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function (data) {
-                        console.log(data)
-                    },
-                error: function (request, status, error) {
-                    console.log("code: " + request.status)
-                    console.log("message: " + request.responseText)
-                    console.log("error: " + error);
-                }
-            }).done(function(resp){
-                console.log(resp);
-                if(resp.status == 500) {
-                    //alert(resp.data);
-                    alert("회원수정에 실패했습니다.");
-                    location.href="/user/info";
-                }else {
-                    alert("회원수정이 완료 되었습니다.");
-                    location.href="/";
-                }
+        $.ajax({
+            type:"PUT",
+            url: "/user/info",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                    console.log(data)
+                },
+            error: function (request, status, error) {
+                console.log("code: " + request.status)
+                console.log("message: " + request.responseText)
+                console.log("error: " + error);
+            }
+        }).done(function(resp){
+            console.log(resp);
+            if(resp.status == 500) {
+                //alert(resp.data);
+                alert("회원수정에 실패했습니다.");
+                location.href="/user/info";
+            }else {
+                alert("회원수정이 완료 되었습니다.");
+                location.href="/";
+            }
 
         }).fail(function(error){
             alert(JSON.stringify(error));
@@ -156,50 +160,68 @@ let index = {
     },
 
     deleteForm: function() {
-                let data = {
-                    id: $("#id").val(),
-                    password : $("#password").val(),
+        let data = {
+            id: $("#id").val(),
+            password : $("#password").val(),
 //                    password_re : $("#password-re").val(),
-                }
+        }
 
 //                if(data.password !== data.password_re) {
 //                    alert("비밀번호가 일치하지 않습니다.")
 //                    return;
 //                }
 
-                if (!confirm("정말 회원 탈퇴를 할까요?")) {
-                    return false;
-                }else {
-                $.ajax({
-                    type:"DELETE",
-                    url: "/user/info",
-                    data: JSON.stringify(data),
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    success: function (data) {
-                            console.log(data)
-                        },
-                    error: function (request, status, error) {
-                        console.log("code: " + request.status)
-                        console.log("message: " + request.responseText)
-                        console.log("error: " + error);
-                    }
-                }).done(function(resp){
-                    console.log(resp);
-                    if(resp.status == 500) {
-                        //alert(resp.data);
-                        alert("회원 탈퇴에 실패했습니다.");
-                        location.href="/user/info";
-                    }else {
-                        alert("회원 탈퇴에 완료 되었습니다.");
-                        location.href="/logout";
-                    }
+        if (!confirm("정말 회원 탈퇴를 할까요?")) {
+            return false;
+        }else {
+        $.ajax({
+            type:"DELETE",
+            url: "/user/info",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                    console.log(data)
+                },
+            error: function (request, status, error) {
+                console.log("code: " + request.status)
+                console.log("message: " + request.responseText)
+                console.log("error: " + error);
+            }
+        }).done(function(resp){
+            console.log(resp);
+            if(resp.status == 500) {
+                //alert(resp.data);
+                alert("회원 탈퇴에 실패했습니다.");
+                location.href="/user/info";
+            }else {
+                alert("회원 탈퇴에 완료 되었습니다.");
+                location.href="/logout";
+            }
 
-                }).fail(function(error){
-                    alert(JSON.stringify(error));
-                });
-                }
-        },
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+        }
+    },
+
+    imgUpload : function() {
+        let file = $('#profile-img')[0].files[0];
+        let formData = new FormData();
+        formData.append("data", file);
+
+        $.ajax({
+            type: "POST",
+            url: "/api/board/imgUpload",
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function (data) {
+            $("#result-profile-image").attr("src", data);
+        }).fail(function (error) {
+            alert(error);
+        })
+    },
 
     chk_form : function() {
         if(document.getElementById("username").value==''){
@@ -211,7 +233,7 @@ let index = {
         	return false;
         }
         document.getElementById('frm').submit();
-    }
+    },
 }
 
 index.init();
