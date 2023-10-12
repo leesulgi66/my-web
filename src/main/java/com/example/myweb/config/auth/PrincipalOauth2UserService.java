c3a1be9dff7d7e198323952b644fa20dba848827
Ori <leesulgi66@naver.com>
2023-10-12 17:18:44 +0900

    PrincipalOauth2UserService.java - 비회원 social user 회원가입

--- src/main/java/com/example/myweb/config/auth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/myweb/config/auth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.example.myweb.config.auth;
 
+import com.example.myweb.model.User;
+import com.example.myweb.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,13 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     //구글로 부터 받은 userRequest 데이터에 대한 후처리용 함수
+
+    @Autowired
+    UserService userService;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        log.info("getClientRegistration : "+userRequest.getClientRegistration());
-        log.info("getAccessToken : "+userRequest.getAccessToken().getTokenValue());
-        log.info("getAttributes : "+super.loadUser(userRequest).getAttributes()); // loadUser함수로 user정보를 받아올 수 있다.
+        //log.info("getClientRegistration : "+userRequest.getClientRegistration());
+        //log.info("getAccessToken : "+userRequest.getAccessToken().getTokenValue());
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        return super.loadUser(userRequest);
+        log.info("getAttributes : "+oAuth2User.getAttributes()); // loadUser함수로 user정보를 받아올 수 있다.  ex: {sub=1234, name=오5리, given_name=5리, family_name=오, picture=https//..., email=leesulgi66@naver.com, email_verified=true, locale=ko}
+
+        String provider = userRequest.getClientRegistration().getClientName(); // google
+
+        User user = userService.OauthUserSave(provider, oAuth2User);
+
+        return new PrincipalDetails(user, oAuth2User.getAttributes());
     }
 }
