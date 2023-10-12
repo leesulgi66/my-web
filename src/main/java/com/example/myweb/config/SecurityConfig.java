7e5ede2ef96e759b4f595bfb89999e6a0140f429
Ori <leesulgi66@naver.com>
2023-10-12 17:17:08 +0900

    SecurityConfig.java - BCryptPasswordEncoder 삭제

--- src/main/java/com/example/myweb/config/SecurityConfig.java
+++ src/main/java/com/example/myweb/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,11 +17,6 @@ public class SecurityConfig {
     private PrincipalDetailsService principalDetailsService;
     @Autowired
     private PrincipalOauth2UserService principalOauth2UserService;
-
-    @Bean
-    BCryptPasswordEncoder encode() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
