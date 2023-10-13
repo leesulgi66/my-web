16c0eaa5b9e2c8b7b5d12359ae0e4a768e2df22b
Ori <leesulgi66@naver.com>
2023-10-13 17:38:39 +0900

    PrincipalOauth2UserService.java - userinfo를 세분화하여 여러가지 소셜 로그인에 대한 회원가입 처리

--- src/main/java/com/example/myweb/config/auth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/myweb/config/auth/PrincipalOauth2UserService.java
@@ -1,5 +1,8 @@
 package com.example.myweb.config.auth;
 
+import com.example.myweb.config.oauth.GoogleUserInfo;
+import com.example.myweb.config.oauth.NaverUserInfo;
+import com.example.myweb.config.oauth.OAuth2UserInfo;
 import com.example.myweb.model.User;
 import com.example.myweb.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -20,15 +25,26 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        //log.info("getClientRegistration : "+userRequest.getClientRegistration());
-        //log.info("getAccessToken : "+userRequest.getAccessToken().getTokenValue());
+        log.info("getClientRegistration : "+userRequest.getClientRegistration());
+        log.info("getAccessToken : "+userRequest.getAccessToken().getTokenValue());
+        log.info("getClientName : "+userRequest.getClientRegistration().getClientName()); // google
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
         log.info("getAttributes : "+oAuth2User.getAttributes()); // loadUser함수로 user정보를 받아올 수 있다.  ex: {sub=1234, name=오5리, given_name=5리, family_name=오, picture=https//..., email=leesulgi66@naver.com, email_verified=true, locale=ko}
 
-        String provider = userRequest.getClientRegistration().getClientName(); // google
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            log.info("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            log.info("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)(oAuth2User.getAttributes().get("response")));
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+            log.info("카카오 로그인 요청");
+        }
 
-        User user = userService.OauthUserSave(provider, oAuth2User);
+        assert oAuth2UserInfo != null;
+        User user = userService.OauthUserSave(oAuth2UserInfo);
 
         return new PrincipalDetails(user, oAuth2User.getAttributes());
     }
