9ffa7cddf03380a4ac289e0d09b55654702065bf
Ori <leesulgi66@naver.com>
2023-10-15 19:11:11 +0900

    SecurityConfig.java - session STATELESS 설정 및 커스텀 필터 등록

--- src/main/java/com/example/myweb/config/SecurityConfig.java
+++ src/main/java/com/example/myweb/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.example.myweb.config;
 
 import com.example.myweb.config.auth.PrincipalDetailsService;
 import com.example.myweb.config.auth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,11 +21,19 @@ public class SecurityConfig {
     private PrincipalDetailsService principalDetailsService;
     @Autowired
     private PrincipalOauth2UserService principalOauth2UserService;
+    @Autowired
+    private CorsConfig corsConfig;
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() //csrf() js로 들어오는 요청을 막아줌. (ajax 요청이 막히기 때문에 임시로 걸어둠)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //인증 처리 관점에서 세션을 생성하지 않음
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new MyCustomDsl()) // 커스텀 필터 등록
+                .and()
                 .authorizeRequests()
                 .antMatchers("/","/auth/**", "/js/**", "/css/**", "/images/**", "/**/*.css")
                 .permitAll()
@@ -30,15 +42,24 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/auth/loginForm") // login page
-                .loginProcessingUrl("/auth/login")  // login 요청주소
-                .defaultSuccessUrl("/") // login 성공시 이동주소
-                .and()
-                .oauth2Login()
-                .loginPage("/auth/loginForm")
-                .userInfoEndpoint()
-                .userService(principalOauth2UserService);
+                .formLogin().disable();
+//                .loginPage("/auth/loginForm") // login page
+//                .loginProcessingUrl("/auth/login")  // login 요청주소
+//                .defaultSuccessUrl("/") // login 성공시 이동주소
+//                .and()
+//                .oauth2Login()
+//                .loginPage("/auth/loginForm")
+//                .userInfoEndpoint()
+//                .userService(principalOauth2UserService);
         return http.build();
+    }
+
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(corsConfig.corsFilter());
+        }
     }
 }
