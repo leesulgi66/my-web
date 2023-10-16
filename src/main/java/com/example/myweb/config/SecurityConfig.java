c53104fd829bac115d749faa5f4626b9639676e4
Ori <leesulgi66@naver.com>
2023-10-16 15:48:17 +0900

    SecurityConfig.java - SecurityConfig에 JwtAuthenticationFilter 등록

--- src/main/java/com/example/myweb/config/SecurityConfig.java
+++ src/main/java/com/example/myweb/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.myweb.config;
 
 import com.example.myweb.config.auth.PrincipalDetailsService;
 import com.example.myweb.config.auth.PrincipalOauth2UserService;
+import com.example.myweb.config.jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +30,7 @@ public class SecurityConfig {
                 .csrf().disable() //csrf() js로 들어오는 요청을 막아줌. (ajax 요청이 막히기 때문에 임시로 걸어둠)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //인증 처리 관점에서 세션을 생성하지 않음
                 .and()
-                .formLogin().disable() // 기본 폼로그인 사용하지 않음
+                .formLogin().disable() // 기본 폼로그인 사용하지 않음 -> 때문에 기본적인 UsernamePasswordAuthenticationFilter가 작동을 하지 않음. 새로운 필터를 등록해야 함.
                 .httpBasic().disable() // 아이디와 비밀번호가 노출되는 basic방식 사용하지 않음
                 .apply(new MyCustomDsl()) // 커스텀 필터 등록
                 .and()
@@ -58,7 +59,8 @@ public class SecurityConfig {
         public void configure(HttpSecurity http) throws Exception {
             AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
             http
-                    .addFilter(corsConfig.corsFilter());
+                    .addFilter(corsConfig.corsFilter())
+                    .addFilter(new JwtAuthenticationFilter(authenticationManager));
         }
     }
 }
