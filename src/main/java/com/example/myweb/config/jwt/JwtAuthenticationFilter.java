e33012046ec1f3472f9dea693a8b2ab584e4854e
Ori <leesulgi66@naver.com>
2023-10-16 15:47:18 +0900

    JwtAuthenticationFilter.java - 로그인 요청을 위한 JwtAuthenticationFilter 생성

---
+++ src/main/java/com/example/myweb/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.example.myweb.config.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    // /login 요청을 하면 로그인 시도를 위해서 실행되는 함수
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("JwtAuthenticationFilter : 로그인 시도중");
+        return super.attemptAuthentication(request, response);
+    }
+}
