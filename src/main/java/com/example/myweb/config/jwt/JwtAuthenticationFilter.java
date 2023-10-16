87bc947a669bf4483012f9d61a79f84fa8599791
Ori <leesulgi66@naver.com>
2023-10-16 17:58:50 +0900

    JwtAuthenticationFilter.java - jwt token 발급

--- src/main/java/com/example/myweb/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/myweb/config/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.example.myweb.config.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.myweb.config.auth.PrincipalDetails;
 import com.example.myweb.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,10 +12,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Date;
 
 @Slf4j
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -50,5 +55,21 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // 위의 attemptAuthentication 실행 후 인증이 정상적으로 되었으면 아래의 successfulAuthentication 함수가 실행됨.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("successfulAuthentication : 인증 완료");
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        String jwtToken = JWT.create()
+                .withSubject(principalDetails.getUsername()) // token name
+                .withExpiresAt(new Date(System.currentTimeMillis()+(1000*60*10)))
+                .withClaim("id", principalDetails.getUser().getId())
+                .sign(Algorithm.HMAC512("secret"));
+
+        response.addHeader("Authorization", "Bearer "+jwtToken);
     }
 }
