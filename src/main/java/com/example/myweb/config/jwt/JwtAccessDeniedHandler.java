cb0a5dedcfeac0d01e185f29cfdc4d8fac386af9
Ori <leesulgi66@naver.com>
2023-10-19 23:47:19 +0900

    JwtAccessDeniedHandler.java

---
+++ src/main/java/com/example/myweb/config/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,17 @@
+package com.example.myweb.config.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        // 필요한 권한이 존재하지 않는 경우 403 FORBIDDEN 에러 리턴
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
