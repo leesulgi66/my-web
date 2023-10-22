ea55ca74465882b1c94356832935172abf71b792
Ori <leesulgi66@naver.com>
2023-10-22 20:22:37 +0900

    test filter - 주석처리

--- src/main/java/com/example/myweb/config/FilterConfig.java
+++ src/main/java/com/example/myweb/config/FilterConfig.java
@@ -1,27 +1,27 @@
-package com.example.myweb.config;
-
-import com.example.myweb.config.filter.Myfilter1;
-import com.example.myweb.config.filter.Myfilter2;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<Myfilter1> filter1() {
-        FilterRegistrationBean<Myfilter1> bean = new FilterRegistrationBean<>(new Myfilter1());
-        bean.addUrlPatterns("/*");
-        bean.setOrder(1); // 낮은 번호가 필터중 가장 먼저 실행됨 하지만, security filter가 모두 실행되고 난 후 실행이 됨.
-        return bean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<Myfilter2> filter2() {
-        FilterRegistrationBean<Myfilter2> bean = new FilterRegistrationBean<>(new Myfilter2());
-        bean.addUrlPatterns("/*");
-        bean.setOrder(0); // 낮은 번호가 필터중 가장 먼저 실행됨
-        return bean;
-    }
-}
+//package com.example.myweb.config;
+//
+//import com.example.myweb.config.filter.Myfilter1;
+//import com.example.myweb.config.filter.Myfilter2;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<Myfilter1> filter1() {
+//        FilterRegistrationBean<Myfilter1> bean = new FilterRegistrationBean<>(new Myfilter1());
+//        bean.addUrlPatterns("/*");
+//        bean.setOrder(1); // 낮은 번호가 필터중 가장 먼저 실행됨 하지만, security filter가 모두 실행되고 난 후 실행이 됨.
+//        return bean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<Myfilter2> filter2() {
+//        FilterRegistrationBean<Myfilter2> bean = new FilterRegistrationBean<>(new Myfilter2());
+//        bean.addUrlPatterns("/*");
+//        bean.setOrder(0); // 낮은 번호가 필터중 가장 먼저 실행됨
+//        return bean;
+//    }
+//}
--- src/main/java/com/example/myweb/config/filter/Myfilter2.java
+++ src/main/java/com/example/myweb/config/filter/Myfilter2.java
@@ -1,12 +1,12 @@
-package com.example.myweb.config.filter;
-
-import javax.servlet.*;
-import java.io.IOException;
-
-public class Myfilter2 implements Filter {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("filter 2");
-        chain.doFilter(request, response); // 프로세스가 끝나지 않게 다시 필터체인으로 넘겨준다.
-    }
-}
+//package com.example.myweb.config.filter;
+//
+//import javax.servlet.*;
+//import java.io.IOException;
+//
+//public class Myfilter2 implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        System.out.println("filter 2");
+//        chain.doFilter(request, response); // 프로세스가 끝나지 않게 다시 필터체인으로 넘겨준다.
+//    }
+//}
