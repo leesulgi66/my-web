f4ef9a6f0825eb9426a2b8cdb757a15b18106f67
Ori <leesulgi66@naver.com>
2023-10-24 17:29:02 +0900

    JwtFilter.java - 임시로 header 가 아닌 cookie 로 로그인 설정

--- src/main/java/com/example/myweb/config/jwt/JwtFilter.java
+++ src/main/java/com/example/myweb/config/jwt/JwtFilter.java
@@ -8,9 +8,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 public class JwtFilter extends OncePerRequestFilter {
@@ -27,6 +29,23 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String jwt = resolveToken(request);
         String requestURI = request.getRequestURI();
+
+        // 임시 쿠키 처리 ----------------------------------------------------------------------------------
+        Cookie[] cookies = request.getCookies();
+        for(Cookie c : cookies) {
+            //System.out.println((c.getName()));  // 쿠키 이름 가져오기
+            //System.out.println((c.getValue()) );  // 쿠키 값 가져오기
+            if(c.getName().equals("Authorization")){
+                if(StringUtils.hasText(c.getValue()) && tokenProvider.validateToken(c.getValue()) == TokenProvider.JwtCode.ACCESS) {
+                    Authentication authentication = tokenProvider.getAuthentication(c.getValue());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    log.info("Cookie : Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+                }else {
+                    log.info("Cookie : 유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+                }
+            }
+        }
+        //-------------------------------------------------------------------------------------------------
 
         if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt) == TokenProvider.JwtCode.ACCESS) {
             Authentication authentication = tokenProvider.getAuthentication(jwt);
