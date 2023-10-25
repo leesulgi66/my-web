17d92e9de4642a40873939618bc2b764f673709a
Ori <leesulgi66@naver.com>
2023-10-25 17:08:28 +0900

    BoardApiController.java - casting을 principal이 아닌 user로 변경

--- src/main/java/com/example/myweb/controller/api/BoardApiController.java
+++ src/main/java/com/example/myweb/controller/api/BoardApiController.java
@@ -1,9 +1,9 @@
 package com.example.myweb.controller.api;
 
-import com.example.myweb.config.auth.PrincipalDetails;
 import com.example.myweb.dto.ReplyDto;
 import com.example.myweb.dto.ResponseDto;
 import com.example.myweb.model.Board;
+import com.example.myweb.model.User;
 import com.example.myweb.service.BoardService;
 import com.example.myweb.service.S3Uploader;
 import lombok.extern.slf4j.Slf4j;
@@ -26,52 +26,52 @@ public class BoardApiController {
     S3Uploader s3Uploader;
 
     @PostMapping("/api/board")
-    public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : save 호출됨");
-        boardService.boardSave(board, principal.getUser());
+        boardService.boardSave(board, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
     @DeleteMapping("/api/board/{id}")
-    public ResponseDto<Integer> delete(@PathVariable Long id, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> delete(@PathVariable Long id, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : delete 호출됨");
-        boardService.boardDelete(id, principal.getUser());
+        boardService.boardDelete(id, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
     @PutMapping("/api/board/{id}")
-    public ResponseDto<Integer> update(@PathVariable Long id, @RequestBody Board board, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> update(@PathVariable Long id, @RequestBody Board board, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : update 호출됨");
-        boardService.boardUpdate(id, board, principal.getUser());
+        boardService.boardUpdate(id, board, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
 
     }
 
     @PostMapping("/api/board/{boardId}/reply")
-    public ResponseDto<Integer> replySave(@PathVariable Long boardId, @RequestBody ReplyDto replyDto, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> replySave(@PathVariable Long boardId, @RequestBody ReplyDto replyDto, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : replySave 호출됨");
-        boardService.replySave(boardId, replyDto, principal.getUser());
+        boardService.replySave(boardId, replyDto, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
     @DeleteMapping("/api/board/{boardId}/reply/{replyId}")
-    public ResponseDto<Integer> replyDelete(@PathVariable Long replyId, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> replyDelete(@PathVariable Long replyId, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : replyDelete 호출됨");
-        boardService.replyDelete(replyId, principal.getUser());
+        boardService.replyDelete(replyId, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
     @PostMapping("/api/reply/{replyId}/replyToComment")
-    public ResponseDto<Integer> replyToCommentSave(@PathVariable Long replyId, @RequestBody ReplyDto replyDto, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> replyToCommentSave(@PathVariable Long replyId, @RequestBody ReplyDto replyDto, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : replyToCommentSave 호출됨");
-        boardService.replyToCommentSave(replyId, replyDto, principal.getUser());
+        boardService.replyToCommentSave(replyId, replyDto, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
     @DeleteMapping("/api/reply/{replyId}/replyToComment/{replyToCommentId}")
-    public ResponseDto<Integer> replyToCommentDelete(@PathVariable Long replyToCommentId, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseDto<Integer> replyToCommentDelete(@PathVariable Long replyToCommentId, @AuthenticationPrincipal User principal) {
         log.info("BoardApiController : replyToCommentDelete 호출됨");
-        boardService.replyToCommentDelete(replyToCommentId, principal.getUser());
+        boardService.replyToCommentDelete(replyToCommentId, principal);
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 
