54dcbfab36910ae18eb8c101a27439c0e207f692
Ori <leesulgi66@naver.com>
2023-11-01 19:42:58 +0900

    WebSocketHandler.java - 채팅방 생성및 맵핑

--- src/main/java/com/example/myweb/chat/WebSocketHandler.java
+++ src/main/java/com/example/myweb/chat/WebSocketHandler.java
@@ -1,5 +1,7 @@
 package com.example.myweb.chat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -8,12 +10,19 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload : {}" ,payload);
-        TextMessage textMessage = new TextMessage("Welcome chatting sever~");
-        session.sendMessage(textMessage);
+
+        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
