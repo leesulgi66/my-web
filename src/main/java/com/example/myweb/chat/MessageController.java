6ade766bd6a9b19a9644cc66dcec66c7e9e5c555
Ori <leesulgi66@naver.com>
2023-11-05 16:37:01 +0900

    MessageController.java - 메시지 발행및 구독자에 전달 컨트롤러

---
+++ src/main/java/com/example/myweb/chat/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.myweb.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+    /*
+        /sub/channel/12345      - 구독(channelId:12345)
+        /pub//chat/message              - 메시지 발행
+    */
+
+    @MessageMapping("/chat/message")
+    public void message(Message message) {
+        if(Message.MessageType.ENTER.equals(message.getType())){
+            message.setMessage(message.getSender()+"님이 입장했습니다.");
+        }
+        simpMessageSendingOperations.convertAndSend("/sub/channel/" + message.getChannelId(), message);
+    }
+}
