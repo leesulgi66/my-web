c5f8088f71fb478d69e63b6ec563e17b025d842b
Ori <leesulgi66@naver.com>
2023-11-08 19:08:35 +0900

    WebSocketConfiguration.java - withSockJS() 추가

--- src/main/java/com/example/myweb/chat/WebSocketConfiguration.java
+++ src/main/java/com/example/myweb/chat/WebSocketConfiguration.java
@@ -18,6 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
