c4b892a90845416b2477e0b0ecc327d466602175
Ori <leesulgi66@naver.com>
2023-11-10 17:20:24 +0900

    WebSocketConfiguration.java - setAllowedOrigins() 특정 주소로 변경.

--- src/main/java/com/example/myweb/chat/WebSocketConfiguration.java
+++ src/main/java/com/example/myweb/chat/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:8000", "http://localhost:3000")
                 .withSockJS();
     }
 }
