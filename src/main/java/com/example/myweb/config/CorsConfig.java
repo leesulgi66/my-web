bac1bd54bb123f629efebc6b068310dd3e83e4c0
Ori <leesulgi66@naver.com>
2023-11-10 17:45:39 +0900

    CorsConfig.java - addAllowedOrigin() 주소로 변경

--- src/main/java/com/example/myweb/config/CorsConfig.java
+++ src/main/java/com/example/myweb/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 서버가 응답을 할 때 json 을 자바스크립트에서 처리할 수 있게 할지를 설정.
-        config.addAllowedOrigin("*"); // 모든 ip의 응답을 허용.
+        config.addAllowedOrigin("\"http://localhost:8000\", \"http://localhost:3000\"");
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용.
         config.addAllowedMethod("*"); // 모든 방식의 method를 허용.
         source.registerCorsConfiguration("/api/**", config);
