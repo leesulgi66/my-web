a0d78a5656c4b9ecd47711082573393d3b37b813
Ori <leesulgi66@naver.com>
2023-11-13 19:22:59 +0900

    ChatRoomRepository.java - 임시 컬렉션 저장에서 DB연결로 변경

--- src/main/java/com/example/myweb/chat/ChatRoomRepository.java
+++ src/main/java/com/example/myweb/chat/ChatRoomRepository.java
@@ -1,36 +1,9 @@
 package com.example.myweb.chat;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.Optional;
 
-@Repository
-public class ChatRoomRepository {
-
-    private Map<String, ChatRoom> chatRoomMap;
-
-    @PostConstruct
-    private void init() {
-        chatRoomMap = new LinkedHashMap<>();
-    }
-
-    public List<ChatRoom> findAllRoom() {
-        // 채팅방 생성순서 최근 순으로 반환
-        List chatRooms = new ArrayList<>(chatRoomMap.values());
-        Collections.reverse(chatRooms);
-        return chatRooms;
-    }
-
-    public ChatRoom findRoomById(String id) {
-        return chatRoomMap.get(id);
-    }
-
-    // service
-    public ChatRoom createChatRoom(String name, String creator) {
-        ChatRoom chatRoom = ChatRoom.create(name);
-        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
-        chatRoom.setCreator(creator);
-        return chatRoom;
-    }
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByRoomId (String roomId);
 }
