83f6f46bb8096d4c05ea3a17a37529c281599406
Ori <leesulgi66@naver.com>
2023-11-15 14:34:47 +0900

    Message.java - dio에서 Entity로 변경

--- src/main/java/com/example/myweb/chat/Message.java
+++ src/main/java/com/example/myweb/chat/Message.java
@@ -1,18 +1,53 @@
 package com.example.myweb.chat;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private MessageType type;
+
+    private String sender;
+
+    private String roomId;
+
+    private String message;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "chatRoomId")
+    private ChatRoom chatRoom;
+
     public enum MessageType {
         ENTER, TALK
     }
-    private MessageType type;
-    private String sender;
-    private String roomId;
-    private String message;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", type=" + type +
+                ", sender='" + sender + '\'' +
+                ", roomId='" + roomId + '\'' +
+                ", message='" + message + '\'' +
+                ", createDate=" + createDate +
+                ", chatRoom=" + chatRoom +
+                '}';
+    }
 }
