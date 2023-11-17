2c1c231eef4fe981dca289f4c94641743f35a4c3
Ori <leesulgi66@naver.com>
2023-11-17 17:16:01 +0900

    RoomAndMessageRepository.java

---
+++ src/main/java/com/example/myweb/chat/RoomAndMessageRepository.java
@@ -0,0 +1,9 @@
+package com.example.myweb.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RoomAndMessageRepository extends JpaRepository <RoomAndMessage, Long> {
+    List<RoomAndMessage> findAllByChatRoom(Long roomId);
+}
