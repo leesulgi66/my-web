73251189748adf07fabdc4b84649f2f41deb6802
Ori <leesulgi66@naver.com>
2023-11-17 17:17:14 +0900

    ChatRoom.java @OneToMany 삭제

--- src/main/java/com/example/myweb/chat/ChatRoom.java
+++ src/main/java/com/example/myweb/chat/ChatRoom.java
@@ -1,12 +1,10 @@
 package com.example.myweb.chat;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.List;
 
 @Builder
 @Getter
@@ -24,11 +22,6 @@ public class ChatRoom {
     private String roomName;
 
     private String creator;
-
-    @OneToMany(mappedBy = "chatRoom", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
-    @JsonIgnoreProperties({"chatRoom"}) // 무한참조 방지
-    @OrderBy("id desc")
-    private List<Message> messages;
 
     @CreationTimestamp
     private Timestamp createDate;
