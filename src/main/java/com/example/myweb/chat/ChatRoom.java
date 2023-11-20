cb83f5710bd3737e4e3c38b287a25d4362fa968c
Ori <leesulgi66@naver.com>
2023-11-20 17:55:00 +0900

    ChatRoom.java, Message.java @OneToMany 설정

--- src/main/java/com/example/myweb/chat/ChatRoom.java
+++ src/main/java/com/example/myweb/chat/ChatRoom.java
@@ -1,10 +1,13 @@
 package com.example.myweb.chat;
 
+import com.example.myweb.model.Reply;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Builder
 @Getter
@@ -22,6 +25,11 @@ public class ChatRoom {
     private String roomName;
 
     private String creator;
+
+    @OneToMany(mappedBy = "chatRoom" ,cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties({"chatRoom"}) // 무한참조 방지
+    @OrderBy("id desc")
+    private List<RoomAndMessage> rooms;
 
     @CreationTimestamp
     private Timestamp createDate;
--- src/main/java/com/example/myweb/chat/Message.java
+++ src/main/java/com/example/myweb/chat/Message.java
@@ -1,10 +1,12 @@
 package com.example.myweb.chat;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,6 +28,11 @@ public class Message {
     private String roomId;
 
     private String message;
+
+    @OneToMany(mappedBy = "message" ,cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties({"message"}) // 무한참조 방지
+    @OrderBy("id desc")
+    private List<RoomAndMessage> messages;
 
     @CreationTimestamp
     private Timestamp createDate;
