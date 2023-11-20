3867686f50421b64ad32e8783c551e7c09d68d48
Ori <leesulgi66@naver.com>
2023-11-20 16:09:22 +0900

    delete

--- src/main/java/com/example/myweb/chat/RoomAndMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myweb.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RoomAndMessageRepository extends JpaRepository <RoomAndMessage, Long> {
-    List<RoomAndMessage> findAllByChatRoom(Long roomId);
-}
