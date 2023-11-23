8f156ae68ed48ea9b24f7020b362a4640ffbb6ca
Ori <leesulgi66@naver.com>
2023-11-23 18:35:15 +0900

    RoomMember.java

---
+++ src/main/java/com/example/myweb/chat/medel/RoomMember.java
@@ -0,0 +1,27 @@
+package com.example.myweb.chat.medel;
+
+import com.example.myweb.model.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RoomMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "CHATROOM_ID")
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+}
