41dd0174e4f963430e49381b691f7b80ffdff1bf
Ori <leesulgi66@naver.com>
2023-11-28 16:44:24 +0900

    RoomMemberRepository.java - room과 user로 조회추가

--- src/main/java/com/example/myweb/chat/repository/RoomMemberRepository.java
+++ src/main/java/com/example/myweb/chat/repository/RoomMemberRepository.java
@@ -2,10 +2,12 @@ package com.example.myweb.chat.repository;
 
 import com.example.myweb.chat.medel.ChatRoom;
 import com.example.myweb.chat.medel.RoomMember;
+import com.example.myweb.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
     List<RoomMember> findAllByChatRoom(ChatRoom chatRoom);
+    RoomMember findByChatRoomAndUser(ChatRoom chatRoom, User user);
 }
