16aad2cacb2065e1d027775db32cb0bd54919481
Ori <leesulgi66@naver.com>
2023-11-29 15:11:35 +0900

    RoomMemberRepository.java - 해당 채팅방과 채팅방 안의 멤버로 삭제

--- src/main/java/com/example/myweb/chat/repository/RoomMemberRepository.java
+++ src/main/java/com/example/myweb/chat/repository/RoomMemberRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
     List<RoomMember> findAllByChatRoom(ChatRoom chatRoom);
     RoomMember findByChatRoomAndUser(ChatRoom chatRoom, User user);
+    void deleteAllByChatRoomAndUser(ChatRoom chatRoom, User user);
 }
